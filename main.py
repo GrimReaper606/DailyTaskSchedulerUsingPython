cec1ea6bdad4d486033b3c07d1cdbcc63dd5de45
GrimReaper606 <avick37@gmail.com>
2023-11-25 17:52:32 +0600

    minor updates

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import typing
-from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.uic import loadUi
 import sys
@@ -8,6 +6,12 @@ class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
         loadUi("main.ui",self)
+        self.calendarWidget.selectionChanged.connect(self.calendarDateChanged)
+    
+    def calendarDateChanged(self):
+        print("The Calendar Date was Changed")
+        dateSelected = self.calendarWidget.selectedDate().toPyDate().strftime("%d-%m")
+        print("Date Selected: ",dateSelected)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

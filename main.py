c4929f150cfb53cf136205cf1e51a45bebe4a1a1
GrimReaper606 <avick37@gmail.com>
2023-12-13 00:05:49 +0600

    First phase done

--- main.py
+++ main.py
@@ -1,28 +1,77 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem
+import sqlite3
+from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem, QMessageBox
 from PyQt5.uic import loadUi
 from PyQt5  import QtCore
 import sys
-
-tasks = ["Emails", "Future work", "pog"]
 
 class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
         loadUi("main.ui",self)
         self.calendarWidget.selectionChanged.connect(self.calendarDateChanged)
-        self.updateTaskList()
+        self.calendarDateChanged()
+        self.saveButton.clicked.connect(self.saveChanges)
+        self.addButton.clicked.connect(self.addNewTask)
 
     def calendarDateChanged(self):
         print("The Calendar Date was Changed")
-        dateSelected = self.calendarWidget.selectedDate().toPyDate().strftime("%d-%m")
+        dateSelected = self.calendarWidget.selectedDate().toPyDate()
         print("Date Selected: ",dateSelected)
-    def updateTaskList(self):
-        for task in tasks:
-            item = QListWidgetItem(task)
+        self.updateTaskList(dateSelected)
+
+    def updateTaskList(self, date):
+        self.listWidget.clear()
+
+        db = sqlite3.connect("data.db")
+        cursor = db.cursor()
+        query = "SELECT task, completed FROM tasks WHERE date = ?"
+        row = (date,)
+        results = cursor.execute(query, row).fetchall()
+        for result in results:
+            item = QListWidgetItem(str(result[0]))
             item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
             item.setCheckState(QtCore.Qt.Unchecked)
+            if result[1] == "YES":
+                item.setCheckState(QtCore.Qt.Checked)
+            elif result[1] == "NO":
+                item.setCheckState(QtCore.Qt.Unchecked)
             self.listWidget.addItem(item)
 
+    def saveChanges(self):
+        db = sqlite3.connect("data.db")
+        cursor = db.cursor()
+        date = self.calendarWidget.selectedDate().toPyDate()
+
+        for i in range(self.listWidget.count()):
+            item = self.listWidget.item(i)
+            task = item.text()
+            if item.checkState() == QtCore.Qt.Checked:
+                query = "UPDATE tasks SET completed = 'YES' WHERE task = ? AND date = ?"
+            else:
+                query = "UPDATE tasks SET completed = 'NO' WHERE task = ? AND date = ?"
+            row = (task, date,)
+            cursor.execute(query, row)
+        db.commit()
+
+        messageBox = QMessageBox()
+        messageBox.setText("Changes Saved.")
+        messageBox.setStandardButtons(QMessageBox.Ok)
+        messageBox.exec()
+    
+    def addNewTask(self):
+        db = sqlite3.connect("data.db")
+        cursor = db.cursor()
+
+        newTask = str(self.taskLineEdit.text())
+        date = self.calendarWidget.selectedDate().toPyDate()
+
+        query = "INSERT INTO tasks(task, completed, date) VALUES (?, ?, ?)"
+        row = (newTask, "NO", date,)
+
+        cursor.execute(query, row)
+        db.commit()
+        self.updateTaskList(date)
+        self.taskLineEdit.clear()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
